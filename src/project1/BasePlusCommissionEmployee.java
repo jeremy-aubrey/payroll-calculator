46caa37c56dffc361905f4317c32af201a8dfc34
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-09 12:30:23 -0600

    added module and method comments

--- src/project1/BasePlusCommissionEmployee.java
+++ src/project1/BasePlusCommissionEmployee.java
@@ -1,19 +1,59 @@
+//********************************************************************
+//
+//  Developer:     Jeremy Aubrey
+//
+//  Project #:     One
+//
+//  File Name:     BasePlusCommissionEmployee.java
+//
+//  Course:        COSC 4301 - Modern Programming
+//
+//  Due Date:      2/13/2022
+//
+//  Instructor:    Fred Kumi 
+//
+//  Description:   BasePlusCommissionEmployee concrete class that inherits from the 
+//                 CommissionEmployee class. BasePlusCommissionEmployee implements the abstract
+//                 earnings method of Employee.
+//
+//********************************************************************
+
 package project1;
 
 public class BasePlusCommissionEmployee extends CommissionEmployee {
 	
 	private double baseSalary;
 	
-	public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate, double baseSalary) {
+	//constructor
+	public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber, 
+			double grossSales, double commissionRate, double baseSalary) 
+	{	
 		super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
 		this.baseSalary = baseSalary;
-	}
+		
+	}//end constructor
 	
+    //***************************************************************
+    //
+    //  Method:       earnings (Non Static)
+    // 
+    //  Description:  Calculates the earnings for a BasePlusCommissionEmployee.
+    //                Earnings are defined as the product of gross sales 
+	//                and commission rate (implemented in super.earnings()) plus
+	//                base salary.
+	//                
+    //  Parameters:   None
+    //
+    //  Returns:      double 
+    //
+    //***************************************************************
 	@Override
 	public double earnings() {
+		
 		System.out.print("Base Salary: $" + String.format("%.2f", baseSalary) + "; ");
 		double earnings = super.earnings() + baseSalary;
 		return earnings;
-	}
+		
+	}//end earnings method
 
 }
