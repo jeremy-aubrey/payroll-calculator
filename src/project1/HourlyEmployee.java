35c7882a47c09ce0f5994301c30c460af72f2162
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-09 12:16:37 -0600

    added module and method comments

--- src/project1/HourlyEmployee.java
+++ src/project1/HourlyEmployee.java
@@ -1,21 +1,59 @@
+//********************************************************************
+//
+//  Developer:     Jeremy Aubrey
+//
+//  Project #:     One
+//
+//  File Name:     HourlyEmployee.java
+//
+//  Course:        COSC 4301 - Modern Programming
+//
+//  Due Date:      2/13/2022
+//
+//  Instructor:    Fred Kumi 
+//
+//  Description:   HourlyEmployee concrete class that inherits from the 
+//                 Employee class. HourlyEmployee implements the abstract
+//                 earnings method of Employee.
+//
+//********************************************************************
+
 package project1;
 
 public class HourlyEmployee extends Employee {
 	
 	private double wage;
 	private double hours;
-
+	
+	//constructor
 	public HourlyEmployee(String firstName, String lastName, String socialSecurityNumber, double wage, double hours) {
+		
 		super(firstName, lastName, socialSecurityNumber);
 		this.wage = wage;
 		this.hours = hours;
-	}
-
+		
+	}//end constructor
+	
+    //***************************************************************
+    //
+    //  Method:       earnings (Non Static)
+    // 
+    //  Description:  Calculates the earnings for a HourlyEmployee.
+    //                Earnings are defined as the product of the 
+	//                hourly wage and the hours worked.
+	//
+    //  Parameters:   None
+    //
+    //  Returns:      double 
+    //
+    //***************************************************************
 	@Override
 	public double earnings() {
+		
 		System.out.println("Hourly wage: $" + wage + "; Hours Worked: " + String.format("%.2f", hours));
 		double earnings = wage * hours;
 		return earnings;
-	}
+		
+	}//end earnings method
 
 }
