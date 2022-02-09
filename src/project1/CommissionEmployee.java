245e2f7948ba13122ff771b9b24b78804e20a2f6
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-09 12:23:19 -0600

    added module and method comments

--- src/project1/CommissionEmployee.java
+++ src/project1/CommissionEmployee.java
@@ -1,21 +1,59 @@
+//********************************************************************
+//
+//  Developer:     Jeremy Aubrey
+//
+//  Project #:     One
+//
+//  File Name:     CommissionEmployee.java
+//
+//  Course:        COSC 4301 - Modern Programming
+//
+//  Due Date:      2/13/2022
+//
+//  Instructor:    Fred Kumi 
+//
+//  Description:   CommissionEmployee concrete class that inherits from the 
+//                 Employee class. CommissionEmployee implements the abstract
+//                 earnings method of Employee.
+//
+//********************************************************************
+
 package project1;
 
 public class CommissionEmployee extends Employee {
 	
 	private double grossSales;
 	private double commissionRate;
-
+	
+	//constructor
 	public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate) {
+		
 		super(firstName, lastName, socialSecurityNumber);
 		this.grossSales = grossSales;
 		this.commissionRate = commissionRate;
-	}
+		
+	}//end constructor
 
+    //***************************************************************
+    //
+    //  Method:       earnings (Non Static)
+    // 
+    //  Description:  Calculates the earnings for a CommissionEmployee.
+    //                Earnings are defined as the product of gross sales 
+	//                and commission rate.
+	//
+    //  Parameters:   None
+    //
+    //  Returns:      double 
+    //
+    //***************************************************************
 	@Override
 	public double earnings() {
+		
 		System.out.println("Gross Sales: $" + String.format("%.2f", grossSales) + "; Commission Rate: " + commissionRate);
 		double earnings = grossSales * commissionRate;
 		return earnings;
-	}
+		
+	}//end earnings method
 
 }
